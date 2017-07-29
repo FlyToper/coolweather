923a6e75acc7a67b3a30421c298c1c4ae6766772
FlyToper <874847721@qq.com>
2017-07-30 04:21:13 +0800

    v1.0

---
+++ app/src/main/java/com/yike/coolweather/Activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.yike.coolweather.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yike.coolweather.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
